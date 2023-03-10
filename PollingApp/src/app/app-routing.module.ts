0322a6cae1793de9af75960d3574cdd9c9040041
Michael Pisman <mike.pisman@gmail.com>
2023-03-10 01:05:24 -0800

    Added authguard to routes

--- PollingApp/src/app/app-routing.module.ts
+++ PollingApp/src/app/app-routing.module.ts
@@ -1,19 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { RegisterComponent } from './components/auth/register/register.component';
 import { WorkspaceListComponent } from './components/workspace-list/workspace-list.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { WorkspaceComponent } from './components/workspace/workspace.component';
+import { AuthGuard } from './auth.guard';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '', redirectTo: 'workspaces', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'workspaces', component: WorkspaceListComponent },
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'workspace/:id', component: WorkspaceComponent }
+  { path: 'workspaces', component: WorkspaceListComponent, canActivate: [AuthGuard] },
+  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
+  { path: 'workspace', component: WorkspaceComponent, canActivate: [AuthGuard] }
 ];
 
 @NgModule({
