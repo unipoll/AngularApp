b910cba8cb327a2ee16259a0830a4fe298f20257
Michael Pisman <mike.pisman@gmail.com>
2023-03-10 00:55:21 -0800

    Added methods to create/edit workspaces

--- PollingApp/src/app/components/workspace-list/workspace-list.component.ts
+++ PollingApp/src/app/components/workspace-list/workspace-list.component.ts
@@ -1,38 +1,55 @@
-import { Component, ViewChild } from '@angular/core';
-import { expand, tap } from 'rxjs/operators';
+import { Component, EventEmitter, OnChanges, OnInit, Output, SimpleChanges, ViewChild } from '@angular/core';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 import { ActivatedRoute, Router } from '@angular/router';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatDialog } from '@angular/material/dialog';
+
 import { ApiService } from '../../services/api.service';
 import { WorkspaceModel } from '../../models/workspace.model';
-import { MatTableDataSource } from '@angular/material/table';
-import { MatPaginator } from '@angular/material/paginator';
-import { MatSort } from '@angular/material/sort';
-import { WorkspaceListModel } from '../../models/workspace-list.model';
+import { WorkspaceService } from '../../services/workspace.service';
+import { CreateWorkspaceComponent } from '../create-workspace/create-workspace.component';
 
 @Component({
   selector: 'app-workspace-list',
   templateUrl: './workspace-list.component.html',
   styleUrls: ['./workspace-list.component.scss']
 })
-export class WorkspaceListComponent {
-  workspaceList!: WorkspaceModel[];
-  displayedColumns: string[] = ['name']; // ['id', 'name', 'description'];
-  displayedColumnsWithOptions = [...this.displayedColumns, 'options']; // 'owner'
+export class WorkspaceListComponent implements OnInit {
+  displayedColumns!: string[];
+  displayedColumnsWithOptions!: string[];
   dataSource!: MatTableDataSource<WorkspaceModel>;
+  workspaceList!: Observable<WorkspaceModel[]>;
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
 
-  constructor(private apiService: ApiService, private router: Router, private route: ActivatedRoute) {
-   }
+  constructor(private apiService: ApiService, private router: Router,
+    private route: ActivatedRoute, private workspaceService: WorkspaceService, private _dialog: MatDialog,) {
+  }
 
-  ngOnInit(): void {
-    this.apiService.getUserWorkspace().pipe(
+  updateWorkspaceList() {
+    this.apiService.getUserWorkspaces().pipe(
       tap((data) => (
         this.dataSource = new MatTableDataSource(data.workspaces),
+        this.workspaceList = this.dataSource.connect(),
         this.dataSource.paginator = this.paginator,
         this.dataSource.sort = this.sort
       ))
     ).subscribe();
+  }
+
+  ngOnInit(): void {
+    if (window.innerWidth > 600) {
+      this.displayedColumns = ['name', 'description', 'owner'];
+    } else {
+      this.displayedColumns = ['name'];
+    }
+    this.displayedColumnsWithOptions = [...this.displayedColumns, 'options']; // 'owner'
+
+    this.updateWorkspaceList();
   }
 
   applyFilter(event: Event) {
@@ -45,6 +62,38 @@ export class WorkspaceListComponent {
   }
 
   onRowClicked(row: any) {
-    this.router.navigate(['/workspace/:id'], { queryParams: { id: row.id }, relativeTo: this.route });
+    this.workspaceService.setData(row);
+    this.router.navigate(['/workspace/'], {
+      queryParams: { id: row.id },
+      relativeTo: this.route,
+      state: {
+        workspace_id: row.id,
+        workspace_name: row.name,
+        workspace_description: row.description,
+        workspace_owner: row.owner
+      }
+    });
+  }
+
+  openCreateForm() {
+    const dialogRef = this._dialog.open(CreateWorkspaceComponent);
+    dialogRef.afterClosed().subscribe({
+      next: (val) => {
+        if (val) {
+          this.updateWorkspaceList();
+        }
+      },
+    });
+  }
+
+  openEditForm(data: any) {
+    const dialogRef = this._dialog.open(CreateWorkspaceComponent, {data,});
+    dialogRef.afterClosed().subscribe({
+      next: (val) => {
+        if (val) {
+          this.updateWorkspaceList();
+        }
+      },
+    });
   }
 }
