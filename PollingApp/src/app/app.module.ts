3bb9334d5b15d6d2be5cac37de929b13834027c2
Michael Pisman <mike.pisman@gmail.com>
2023-03-10 01:10:19 -0800

    Added component to create/edit workspace Added providers for services: API, Auth, Sidenav, Workspace, SnackBar Added interceptor for authguard

--- PollingApp/src/app/app.module.ts
+++ PollingApp/src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { MaterialModule } from './material/material.module';
 
-import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { RegisterComponent } from './components/auth/register/register.component';
 import { WorkspaceListComponent } from './components/workspace-list/workspace-list.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { LayoutModule } from '@angular/cdk/layout';
@@ -16,7 +16,13 @@ import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { HeaderComponent } from './components/header/header.component';
 import { SidenavService } from './services/sidenav.service';
 import { WorkspaceComponent } from './components/workspace/workspace.component';
-
+import { AuthGuard } from './auth.guard';
+import { AuthService } from './services/auth.service';
+import { WorkspaceService } from './services/workspace.service';
+import { ApiService } from './services/api.service';
+import { TokenInterceptorService } from './services/token-interceptor.service';
+import { CreateWorkspaceComponent } from './components/create-workspace/create-workspace.component';
+import { SnackBarService } from './services/snackbar.service';
 
 @NgModule({
   declarations: [
@@ -28,6 +34,7 @@ import { WorkspaceComponent } from './components/workspace/workspace.component';
     SidebarComponent,
     HeaderComponent,
     WorkspaceComponent,
+    CreateWorkspaceComponent
   ],
   imports: [
     BrowserModule,
@@ -37,7 +44,19 @@ import { WorkspaceComponent } from './components/workspace/workspace.component';
     LayoutModule,
     MaterialModule
   ],
-  providers: [SidenavService],
+  providers: [
+    ApiService,
+    AuthService,
+    AuthGuard,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptorService,
+      multi: true
+    },
+    SidenavService,
+    WorkspaceService,
+    SnackBarService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
