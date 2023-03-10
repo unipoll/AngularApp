bcc11efae4b8992c2026f2a718b6b31ac830e9fa
Michael Pisman <mike.pisman@gmail.com>
2023-03-10 00:47:06 -0800

    Created a service for MatSnackBar

---
+++ PollingApp/src/app/services/snackbar.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SnackBarService {
+
+  constructor(private _snackBar: MatSnackBar) { }
+
+  openSnackBar(message: string, action: string = 'ok') {
+    this._snackBar.open(message, action, {
+      duration: 1000,
+      verticalPosition: 'top',
+    });
+  }
+}
