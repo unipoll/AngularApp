a55366a9031a1b5cfda3dabf8a10d555be494a54
Michael Pisman <mike.pisman@gmail.com>
2023-08-17 19:16:54 -0600

    feat: Added workspace field of type WorkspaceModel

--- src/app/models/group.model.ts
+++ src/app/models/group.model.ts
@@ -1,9 +1,11 @@
 import { MemberListModel } from "./member.model";
+import { WorkspaceModel } from "./workspace.model";
 
 export interface GroupModel {
     id: string;
     name: string;
     description: string;
+    workspace: WorkspaceModel;
     members: MemberListModel
 }
 
