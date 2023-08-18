9e5d5980cd2be2aa05d598319c76cd5183387051
Michael Pisman <mike.pisman@gmail.com>
2023-08-17 19:11:29 -0600

    feat: Added function to get group and user permissions
    Added function to get group information and user permissions inside
    group

--- src/app/components/group/group.component.ts
+++ src/app/components/group/group.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
-import { tap } from 'rxjs';
+import { lastValueFrom, tap } from 'rxjs';
 import { GroupModel } from '../../models/group.model';
 import { MemberModel } from '../../models/member.model';
 import { WorkspaceModel } from '../../models/workspace.model';
 import { ApiService } from '../../services/api.service';
 import { WorkspaceService } from '../../services/workspace.service';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-group',
@@ -13,31 +14,56 @@ import { WorkspaceService } from '../../services/workspace.service';
   styleUrls: ['./group.component.scss']
 })
 export class GroupComponent {
-  public workspace!: WorkspaceModel | null;
+
+  @Input() workspace!: WorkspaceModel;
+
+  // public workspace!: WorkspaceModel | null;
   public group!: GroupModel | null;
+  public memberListData!: MemberModel[];
+  public group_id!: string;
+  public permissions!: string[];
 
-  memberListData!: MemberModel[];
+  // Markers for permissions
+  public can_get_members: boolean = false;
+  public can_add_members: boolean = false;
+  public can_get_policies: boolean = false;
+  public can_set_policies: boolean = false;
+  public can_delete_group: boolean = false;
 
-  public workspace_id!: string;
-
-  constructor(private router: Router, private route: ActivatedRoute, private apiService: ApiService,
-    private workspaceService: WorkspaceService) { }
-
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private apiService: ApiService,
+    private workspaceService: WorkspaceService,
+    private authService: AuthService) {
+  }
 
   ngOnInit(): void {
-    // this.route.queryParams.subscribe(params => {
-    //   this.group_id = params['id'];
-    // });
-    this.group = this.workspaceService.getGroup();
-    this.workspace = this.workspaceService.getWorkspace();
+    this.route.queryParams.subscribe(params => {
+      this.group_id = params['id'];
+    });
+    
+    this.getGroup();
+  }
 
-    if (this.group == null) {
-      this.router.navigate
-    }
-    else {
-        this.apiService.getGroup(this.group.id).pipe(tap((data: GroupModel | null) => (
-          this.group = data
-        ))).subscribe();
-    }
+  async getGroup(): Promise<void> {
+    let group = this.workspaceService.getGroup();
+    console.log("Group from service", this.group);
+
+    if (!group)
+      group = await lastValueFrom(this.apiService.getGroup(this.group_id));
+
+    this.group = group;
+    this.workspace = group.workspace;
+
+    await lastValueFrom(this.apiService.getGroupPolicy(group.id)).then((response: any) => {
+      console.log("Group policy", response);
+      this.permissions = response.permissions;
+      this.can_get_members = this.permissions.includes('get_group_members');
+      this.can_add_members = this.permissions.includes('add_group_members');
+      this.can_get_policies = this.permissions.includes('get_group_policies');
+      this.can_set_policies = this.permissions.includes('set_group_policies');
+      this.authService.setPermissions(this.permissions);
+    })
   }
 }
