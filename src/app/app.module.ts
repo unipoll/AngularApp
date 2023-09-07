f0445fee66738425bb34effe9f60076dbe6f3f0a
Michael Pisman <mike.pisman@gmail.com>
2023-09-06 21:43:21 -0600

    refactor: Moved material module inside modules folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,25 @@
+// Modules
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-
-import { AppComponent } from './app.component';
+import { LayoutModule } from '@angular/cdk/layout';
 import { AppRoutingModule } from './app-routing.module';
-import { MaterialModule } from './material/material.module';
+import { MaterialModule } from './modules/material/material.module';
 
+// Guards
+import { AuthGuard } from './auth.guard';
+
+// Components
+import { AppComponent } from './app.component';
 import { LoginComponent } from './components/auth/login/login.component';
 import { RegisterComponent } from './components/auth/register/register.component';
 import { WorkspaceListComponent } from './components/workspace-list/workspace-list.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { LayoutModule } from '@angular/cdk/layout';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { HeaderComponent } from './components/header/header.component';
-import { SidenavService } from './services/sidenav.service';
 import { WorkspaceComponent } from './components/workspace/workspace.component';
 import { GroupComponent } from './components/group/group.component';
-import { AuthGuard } from './auth.guard';
-import { AuthService } from './services/auth.service';
-import { WorkspaceService } from './services/workspace.service';
-import { ApiService } from './services/api.service';
-import { TokenInterceptorService } from './services/token-interceptor.service';
-import { SnackBarService } from './services/snackbar.service';
 import { MemberListComponent } from './components/member-list/member-list.component';
 import { GroupListComponent } from './components/group-list/group-list.component';
 import { AddMemberComponent } from './components/add-member/add-member.component';
@@ -33,6 +30,15 @@ import { DialogAddMemberComponent } from './components/dialogs/dialog-add-member
 import { DialogAddPolicyComponent } from './components/dialogs/dialog-add-policy/dialog-add-policy.component';
 import { PolicyListComponent } from './components/policy-list/policy-list.component';
 import { DialogSetPolicyComponent } from './components/dialogs/dialog-set-policy/dialog-set-policy.component';
+
+// Services
+import { SidenavService } from './services/sidenav.service';
+import { AuthService } from './services/auth.service';
+import { WorkspaceService } from './services/workspace.service';
+import { ApiService } from './services/api.service';
+import { TokenInterceptorService } from './services/token-interceptor.service';
+import { SnackBarService } from './services/snackbar.service';
+
 
 @NgModule({
   declarations: [
