33d447f458ba640b75d7b642f50cd5098a624578
Michael Pisman <mike.pisman@gmail.com>
2023-09-06 22:21:51 -0600

    refactor: Removed garbage components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/auth/login/login.component';
 import { RegisterComponent } from './components/auth/register/register.component';
 import { WorkspaceListComponent } from './components/workspace-list/workspace-list.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { WorkspaceComponent } from './components/workspace/workspace.component';
 import { AuthGuard } from './auth.guard';
 import { GroupListComponent } from './components/group-list/group-list.component';
@@ -15,7 +14,6 @@ const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'workspaces', component: WorkspaceListComponent, canActivate: [AuthGuard] },
-  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
   { path: 'workspace', component: WorkspaceComponent, canActivate: [AuthGuard] },
   { path: 'group', component: GroupComponent, canActivate: [AuthGuard]}
 ];
