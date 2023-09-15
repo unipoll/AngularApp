b372fef0ec172ef01d7d3b176768f58ba84b313a
Michael Pisman <mike.pisman@gmail.com>
2023-09-14 22:59:58 -0600

    feat: Added name and description fields to poll editor

--- src/app/components/poll-editor/poll-editor.component.ts
+++ src/app/components/poll-editor/poll-editor.component.ts
@@ -1,13 +1,18 @@
 import { Component, ComponentRef, Input, ViewChild, ViewContainerRef } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Location } from '@angular/common';
 
 import { SingleChoiceComponent } from './questions/single-choice/single-choice.component';
 import { MultipleChoiceComponent } from './questions/multiple-choice/multiple-choice.component';
 import { AddQuestionComponent } from '../dialogs/add-question/add-question.component';
 import { DialogCancelComponent } from '../dialogs/cancel/cancel.component';
-
+import { WorkspaceService } from 'src/app/services/workspace.service';
+import { WorkspaceModel } from 'src/app/models/workspace.model';
+import { ApiService } from 'src/app/services/api.service';
+import { NewPollRequestBody, PollModel } from 'src/app/models/poll.model';
+import { QuestionModel } from 'src/app/models/question.model';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-poll-editor',
@@ -18,9 +23,29 @@ export class PollEditorComponent {
   @ViewChild('pollsContainer', { read: ViewContainerRef }) pollsContainer!: ViewContainerRef;
 
   polls: ComponentRef<any>[] = [];
-  
-  constructor(private dialog: MatDialog, private router: Router,
-    private location: Location) { }
+  private workspace_id: string;
+
+  newPollForm = new FormGroup({
+    name: new FormControl('', Validators.required),
+    description: new FormControl(''),
+    public: new FormControl(false, Validators.required),
+    published: new FormControl(false, Validators.required),
+  });
+
+  constructor(
+    private dialog: MatDialog, 
+    private router: Router,
+    private location: Location,
+    private workspaceService: WorkspaceService,
+    private apiService: ApiService,
+  ) {
+    // this.workspace = this.workspaceService.getWorkspace();
+    this.workspace_id = this.router.getCurrentNavigation()?.extras.state?.['workspace_id'];
+    if (!this.workspace_id) {
+      console.log("No workspace_id");
+      this.location.back();
+    }
+  }
 
   addQuestion() {
     const dialogRef = this.dialog.open(AddQuestionComponent);
@@ -74,7 +99,8 @@ export class PollEditorComponent {
       console.log("No polls");
       return;
     }
-    let pollData = [];
+    let newPoll: NewPollRequestBody;
+    let questions: QuestionModel[] = [];
     for (let poll of this.polls) {
       poll.instance.questionForm.markAllAsTouched();
       // poll.instance.questionForm.updateValueAndValidity();
@@ -82,9 +108,23 @@ export class PollEditorComponent {
         console.log("Invalid form");
         return;
       }
-      pollData.push(poll.instance.getQuestion());
+      questions.push(poll.instance.getQuestion());
     }
-    console.log(JSON.stringify(pollData));
+    newPoll = {
+      name: this.newPollForm.get('name')?.value as string,
+      description: this.newPollForm.get('description')?.value as string,
+      public: this.newPollForm.get('public')?.value as boolean,
+      published: this.newPollForm.get('published')?.value  as boolean,
+      questions: questions,
+    };
+    // console.log(JSON.stringify(newPoll));
+
+    this.apiService.createPoll(this.workspace_id, newPoll).subscribe(
+      (response: PollModel) => {
+        console.log(response);
+        this.location.back();
+      }
+    );
   }
 
   cancel() {
