efcfd79548babd487d69db51e7872fd51ab3fe54
Michael Pisman <mike.pisman@gmail.com>
2023-12-02 23:40:11 -0700

    style: Spaces

--- src/app/core/services/snackbar.service.ts
+++ src/app/core/services/snackbar.service.ts
@@ -1,16 +1,16 @@
 import { Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class SnackBarService {
 
-  constructor(private snackBar: MatSnackBar) { }
+    constructor(private snackBar: MatSnackBar) { }
 
-  openSnackBar(message: string, action: string = 'ok', color: string = 'success') {
-    this.snackBar.open(message, action, {
-      duration: 1000,
-      verticalPosition: 'top',
-      panelClass: 'app-notification-' + color,
-    });
-  }
+    openSnackBar(message: string, action: string = 'ok', color: string = 'success') {
+        this.snackBar.open(message, action, {
+            duration: 1000,
+            verticalPosition: 'top',
+            panelClass: 'app-notification-' + color,
+        });
+    }
 }
