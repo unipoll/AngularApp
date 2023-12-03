31001cc44b3cdf406d590e65d8752f90f0a4d2fb
Michael Pisman <mike.pisman@gmail.com>
2023-12-02 23:43:59 -0700

    refactor: Added GustOnly guards

--- src/app/modules/auth/auth-routing.module.ts
+++ src/app/modules/auth/auth-routing.module.ts
@@ -2,14 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { guestOnly } from 'src/app/core/guards/auth.guard';
 
 const routes: Routes = [{ 
         path: 'login', 
-        component: LoginComponent 
+        component: LoginComponent,
+        canActivate: [guestOnly]
     },
     { 
         path: 'register', 
-        component: RegisterComponent 
+        component: RegisterComponent,
+        canActivate: [guestOnly]
     }
 ];
 
