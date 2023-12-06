f6dd3150190d39a6893c3bdf5466084bfe25156d
Michael Pisman <mike.pisman@gmail.com>
2023-12-05 23:10:17 -0700

    feat: Added dialog to add workspace members

---
+++ src/app/modules/workspace/components/dialog-add-member/dialog-add-member.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DialogAddMemberComponent } from './dialog-add-member.component';
+
+describe('DialogAddMemberComponent', () => {
+  let component: DialogAddMemberComponent;
+  let fixture: ComponentFixture<DialogAddMemberComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [DialogAddMemberComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(DialogAddMemberComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/modules/workspace/components/dialog-add-member/dialog-add-member.component.ts
@@ -0,0 +1,129 @@
+import { Component, ElementRef, Inject, ViewChild, inject } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { ApiService } from 'src/app/core/services/api.service';
+import { SnackBarService } from 'src/app/core/services/snackbar.service';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { startWith, map, Observable } from 'rxjs';
+import { LiveAnnouncer } from '@angular/cdk/a11y';
+import { ENTER, COMMA } from '@angular/cdk/keycodes';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
+import { AccountModel } from 'src/app/models/account.model';
+import { MemberModel } from 'src/app/models/member.model';
+import { WorkspaceModel } from 'src/app/models/workspace.model';
+
+@Component({
+    selector: 'dialog-add-member',
+    templateUrl: './dialog-add-member.component.html',
+    styleUrl: './dialog-add-member.component.scss'
+})
+export class DialogAddMemberComponent {
+    form = new FormGroup({
+        name: new FormControl("", Validators.required),
+        description: new FormControl("", Validators.required),
+    });
+
+    title = "Add member to the workspace";
+    buttons = [
+        {
+            text: "Cancel",
+            color: "basic",
+            action: () => {
+                this.dialog.close(false);
+            }
+        },
+        {
+            text: "Invite",
+            color: "primary",
+            action: this.addMember.bind(this)
+        }
+    ];
+
+    workspace!: WorkspaceModel;
+    separatorKeysCodes: number[] = [ENTER, COMMA];
+    accountCtrl = new FormControl<string | MemberModel>('');
+    filteredAccounts: Observable<MemberModel[]>;
+    accounts: MemberModel[] = [];
+    allAccounts: AccountModel[] = [];
+
+    @ViewChild('accountInput')
+    accountInput!: ElementRef<HTMLInputElement>;
+
+    announcer = inject(LiveAnnouncer);
+
+    constructor(
+        private dialog: MatDialogRef<DialogAddMemberComponent>,
+        private apiService: ApiService,
+        private snackBarService: SnackBarService,
+        @Inject(MAT_DIALOG_DATA) private data: any
+    ) {
+        this.workspace = this.data.workspace;
+
+        this.allAccounts = this.data.accountList.filter(
+            ({ id: account_id }: { id: string }) => (!this.data.memberList.some(({ account_id: member_account_id }: { account_id: string }) => member_account_id === account_id))
+        );
+        this.filteredAccounts = this.accountCtrl.valueChanges.pipe(
+            startWith(''),
+            map(value => {
+                const name = typeof value === 'string' ? value : value?.first_name;
+                return name ? this.filter(name as string) : this.allAccounts.slice();
+            }),
+            // map((filter_field: string | null) => (filter_field ? this._filter(filter_field) : this.allAccounts))
+        );
+    }
+
+    displayFn(account: MemberModel): string {
+        return account && account.first_name ? account.first_name : '';
+    }
+
+    // When a chip is removed
+    remove(member: MemberModel): void {
+        const index = this.accounts.indexOf(member);
+        // console.log("Index", index);
+
+        if (index >= 0) {
+            this.accounts.splice(index, 1);   // Remove chip
+            this.allAccounts.push(member);    // Add back to the autocomplete list
+            this.accountCtrl.setValue(null);  // Clear the input value to reset the autocomplete
+            this.announcer.announce(`Removed ${member}`);
+        }
+    }
+
+    // When item is selected from the list
+    selected(event: MatAutocompleteSelectedEvent): void {
+        this.accounts.push(event.option.value);
+        let index = this.allAccounts.indexOf(event.option.value);
+        this.allAccounts.splice(index, 1);  // Remove from autocomplete list
+
+        this.accountInput.nativeElement.value = '';
+        this.accountCtrl.setValue(null);
+    }
+
+    // Filter the autocomplete list
+    filter(value: string): MemberModel[] {
+        // console.log("Filter Value", value);
+        const filterValue = value.toLowerCase();
+        return this.allAccounts.filter((account: MemberModel) => (
+            account.first_name.toLowerCase().includes(filterValue) ||
+            account.last_name.toLowerCase().includes(filterValue) ||
+            account.email.toLowerCase().includes(filterValue)));
+    }
+
+    addMember() {
+        // Create array of account ids
+        let newMemberIDs = this.accounts.map((account: MemberModel) => account.id);
+        let req = { "accounts": newMemberIDs };
+
+        if (this.accounts.length > 0) {
+            this.apiService.addMemberToWorkspace(this.workspace.id, req).subscribe({
+                next: (val: any) => {
+                    this.snackBarService.openSnackBar('Members added successfully');
+                    this.dialog.close(val);
+                },
+                error: (err: any) => {
+                    this.snackBarService.openSnackBar('Error adding members: err', 'ok', 'error');
+                    console.error(err);
+                },
+            });
+        }
+    }
+}
