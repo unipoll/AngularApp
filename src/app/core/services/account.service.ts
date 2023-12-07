5d3633fb6e93351b4768cbacf0ba6748c084b5ad
Michael Pisman <mike.pisman@gmail.com>
2023-12-06 23:28:30 -0700

    refactor: Refactored account service
    Changed account data type

--- src/app/core/services/account.service.ts
+++ src/app/core/services/account.service.ts
@@ -9,31 +9,37 @@ import { ApiService } from './api.service';
 })
 export class AccountService {
 
-    private _account$ = new BehaviorSubject<AccountModel | null>(null);
-    account$ = this._account$.asObservable();
+    // private _account$ = new BehaviorSubject<AccountModel | null>(null);
+    // account$ = this._account$.asObservable();
+
+    private account!: AccountModel;
 
     constructor(private apiService: ApiService) {
-        const account = localStorage.getItem('userAccount');
-        this._account$.next(account ? JSON.parse(account) : null);
+        const stored_account = localStorage.getItem('userAccount');
+        // this._account$.next(account ? JSON.parse(account) : null);
+        this.account = stored_account ? JSON.parse(stored_account) : this.loadAccount();
     }
 
     getAccount() {
-        return this._account$.value;
+        // return this._account$.value;
+        return this.account;
     }
 
     setAccount(account: AccountModel) {
-        this._account$.next(account);
+        // this._account$.next(account);
+        this.account = account;
     }
 
     loadAccount() {
         this.apiService.getUserAccount().subscribe((account: AccountModel) => {
-            this._account$.next(account);
+            // this._account$.next(account);
+            this.account = account;
             localStorage.setItem('userAccount', JSON.stringify(account));
         });
     }
 
     clearAccount() {
-        this._account$.next(null);
+        // this._account$.next(null);
         localStorage.removeItem('userAccount');
     }
 }
