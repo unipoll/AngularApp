4b486ff0451e892aad3b01571c2e916ed4a569c8
Michael Pisman <mike.pisman@gmail.com>
2023-12-07 11:10:31 -0700

    feat: Improved tabs functionality
    Added route data to determine the current tab based on url and function to update url on tab change

--- src/app/modules/workspace/components/workspace/workspace.component.ts
+++ src/app/modules/workspace/components/workspace/workspace.component.ts
@@ -1,5 +1,6 @@
 import { concatMap, lastValueFrom, switchMap, tap } from 'rxjs';
-import { Component, Input, ViewChild } from '@angular/core';
+import { Location } from '@angular/common'; 
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { WorkspaceModel } from 'src/app/models/workspace.model';
 import { ApiService } from 'src/app/core/services/api.service';
 import { AuthorizationService } from 'src/app/core/services/authorization.service';
@@ -9,29 +10,27 @@ import { MemberListComponent } from '../member-list/member-list.component';
 import { GroupListComponent } from '../group-list/group-list.component';
 import { PolicyListModel } from 'src/app/models/policy.model';
 import { MemberModel } from 'src/app/models/member.model';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
     selector: 'app-workspace',
     templateUrl: './workspace.component.html',
     styleUrls: ['./workspace.component.scss']
 })
-export class WorkspaceComponent {
+export class WorkspaceComponent implements OnInit {
     // Workspace data
     workspace!: WorkspaceModel;
     member!: MemberModel;
-    // public permissions!: string[];
 
-    // TODO: Make permissions an observable
-    /* Update page on permission change
-      i.e. On Load
-      private _can_get_groups$ = new BehaviorSubject<boolean>(false);
-      public can_get_groups$ = this._can_get_groups$.asObservable();
-      ...
-      On change:
-      this._can_get_groups$.next(this.permissions.includes('get_groups'));
-    */
-
-    @Input() id!: string;
+    @Input() workspace_id!: string;
+    
+    tab: string = 'polls';
+    tabs: {[key: string]: number} = {
+        'polls': 0,
+        'policies': 1,
+        'members': 2,
+        'groups': 3
+    }
     
     @ViewChild(GroupListComponent) groupList!: GroupListComponent;
     @ViewChild(MemberListComponent) memberList!: MemberListComponent;
@@ -40,17 +39,26 @@ export class WorkspaceComponent {
     // Constructor
     constructor(
         private apiService: ApiService,
+        private activatedRoute: ActivatedRoute,
+        private location: Location,
         private authService: AuthorizationService,
         private accountService: AccountService) { }
 
     // Get workspace data on init
     ngOnInit(): void {
+
         this.getWorkspace();
+
+        this.activatedRoute.data.subscribe(data => {
+            if (data['tab']) {
+                this.tab = data['tab'];
+            }
+        });
     }
 
     // Get workspace data
     getWorkspace(): void {
-        this.apiService.getWorkspace(this.id, true, true, true, true).subscribe(workspace => {
+        this.apiService.getWorkspace(this.workspace_id, true, true, true, true).subscribe(workspace => {
             workspace.members.forEach((member: MemberModel) => {
                 if (member.account_id == this.accountService.getAccount().id) {
                     this.member = member;
@@ -66,7 +74,6 @@ export class WorkspaceComponent {
     isAllowed(permission: string): boolean {
         return this.authService.isAllowed(permission);
     }
-
 
     // Handle Events
     eventHandler(event: any): void {
@@ -89,9 +96,13 @@ export class WorkspaceComponent {
         }
     }
 
-
     // Update Policy List
     updatePolicyList(): void {
         this.policyList.updatePolicyList();
+    }
+
+    // Update Member List
+    onTabChange(event: any) {
+        this.location.replaceState(`/workspaces/${this.workspace_id}/${event.tab.textLabel.toLowerCase()}`);
     }
 }
