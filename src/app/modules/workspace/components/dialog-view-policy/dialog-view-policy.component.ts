c0d4d2e983d4007e5fef40ebc462a3f0a5215758
Michael Pisman <mike.pisman@gmail.com>
2023-12-08 23:47:38 -0700

    refactor: Updated title for the dialog

--- src/app/modules/workspace/components/dialog-view-policy/dialog-view-policy.component.ts
+++ src/app/modules/workspace/components/dialog-view-policy/dialog-view-policy.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, Input } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { GroupModel } from 'src/app/models/group.model';
 import { MemberModel } from 'src/app/models/member.model';
@@ -9,19 +9,19 @@ import { PolicyModel } from 'src/app/models/policy.model';
     templateUrl: './dialog-view-policy.component.html',
     styleUrl: './dialog-view-policy.component.scss'
 })
-export class DialogViewPolicyComponent {
+export class DialogViewPolicyComponent{
 
     title: string = "Policy";
     policy: PolicyModel;
 
     constructor(@Inject(MAT_DIALOG_DATA) private data: { policy: PolicyModel } ) {
         this.policy = this.data.policy;
-        if (this.policy.policy_holder_type == 'group') {
+        if (this.policy.policy_holder_type == 'Group') {
             let group = this.policy.policy_holder as GroupModel;
-            this.title = "Policy for Group: " + group.name;
-        } else if (this.policy.policy_holder_type == 'member') {
+            this.title = `Policy for Group ${group.name}`;
+        } else if (this.policy.policy_holder_type == 'Member') {
             let member = this.policy.policy_holder as MemberModel;
-            this.title = "Policy for Member: " + member.full_name;
+            this.title = `Policy for Member" ${member.first_name} ${member.last_name}`;
         }
     }
 }
