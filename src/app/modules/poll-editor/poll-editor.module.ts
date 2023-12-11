2c2109c14d0a442428b0467c19e1c3f0589006ee
Michael Pisman <mike.pisman@gmail.com>
2023-12-10 23:25:12 -0700

    feat: Added poll editor page, dialogs to add questions, and exit the editor

--- src/app/modules/poll-editor/poll-editor.module.ts
+++ src/app/modules/poll-editor/poll-editor.module.ts
@@ -1,25 +1,35 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SingleChoiceComponent } from '../../components/poll-editor/questions/single-choice/single-choice.component';
-import { MultipleChoiceComponent } from '../../components/poll-editor/questions/multiple-choice/multiple-choice.component';
+import { SingleChoiceComponent } from './components/questions/single-choice/single-choice.component';
+import { MultipleChoiceComponent } from './components/questions/multiple-choice/multiple-choice.component';
 import { MaterialModule } from '../material/material.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MarkdownModule } from 'ngx-markdown';
+import { PollEditorComponent } from './components/poll-editor/poll-editor.component';
+import { PollEditorRoutingModule } from './poll-editor-routing.module';
+import { DialogAddQuestionComponent } from './components/dialog-add-question/dialog-add-question.component';
+import { DialogExitComponent } from './components/dialog-exit/dialog-exit.component';
+import { DialogComponent } from 'src/app/shared/components/dialog/dialog.component';
+
 
 @NgModule({
-  declarations: [
-    SingleChoiceComponent,
-    MultipleChoiceComponent
-  ],
-  imports: [
-    CommonModule,
-    MaterialModule,
-    ReactiveFormsModule,
-    MarkdownModule.forChild()
-  ],
-  exports: [
-    SingleChoiceComponent,
-    MultipleChoiceComponent
-  ]
+    declarations: [
+        PollEditorComponent,
+        DialogAddQuestionComponent,
+        DialogExitComponent,
+        SingleChoiceComponent,
+        MultipleChoiceComponent
+    ],
+    imports: [
+        CommonModule,
+        MaterialModule,
+        ReactiveFormsModule,
+        PollEditorRoutingModule,
+        MarkdownModule.forChild(),
+        DialogComponent
+    ],
+    exports: [
+        PollEditorComponent
+    ]
 })
 export class PollEditorModule { }
