be16fd04d2a58664c7170c54388826200291157c
Michael Pisman <mike.pisman@gmail.com>
2023-12-12 22:59:15 -0700

    feat: Added group page component

---
+++ src/app/modules/group/components/group/group.component.ts
@@ -0,0 +1,111 @@
+import { Component, Input, ViewChild } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { GroupModel } from 'src/app/models/group.model';
+import { MemberModel } from 'src/app/models/member.model';
+import { WorkspaceModel } from 'src/app/models/workspace.model';
+import { ApiService } from 'src/app/core/services/api.service';
+import { AuthorizationService } from 'src/app/core/services/authorization.service';
+import { MemberListComponent } from '../member-list/member-list.component';
+import { PolicyListComponent } from '../policy-list/policy-list.component';
+import { AccountService } from 'src/app/core/services/account.service';
+
+@Component({
+    selector: 'app-group',
+    templateUrl: './group.component.html',
+    styleUrls: ['./group.component.scss']
+})
+export class GroupComponent {
+
+    @Input() workspace_id!: WorkspaceModel;
+    @Input() group_id!: string;
+
+    workspace!: WorkspaceModel;
+    group!: GroupModel;
+    member!: MemberModel;
+
+    tab: string = 'polls';
+    tabs: { [key: string]: number } = {
+        'policies': 0,
+        'members': 1
+    }
+
+    @ViewChild(MemberListComponent) memberList!: MemberListComponent;
+    @ViewChild(PolicyListComponent) policyList!: PolicyListComponent;
+
+    // public permissions!: string[];
+
+    // Markers for permissions
+    // public can_get_members: boolean = false;
+    // public can_add_members: boolean = false;
+    // public can_get_policies: boolean = false;
+    // public can_set_policies: boolean = false;
+    // public can_delete_group: boolean = false;
+
+    constructor(
+        private activatedRoute: ActivatedRoute,
+        private location: Location,
+        private apiService: ApiService,
+        private accountService: AccountService,
+        private authService: AuthorizationService) {
+    }
+
+    ngOnInit(): void {
+        this.getGroup();
+
+        this.activatedRoute.data.subscribe(data => {
+            if (data['tab']) {
+                this.tab = data['tab'];
+            }
+        });
+    }
+
+    getGroup(): void {
+        this.apiService.getGroup(this.group_id, true, true).subscribe(group => {
+            this.group = group;
+            console.log('group: ', group);
+            // workspace.members.forEach((member: MemberModel) => {
+            //     if (member.account_id == this.accountService.getAccount().id) {
+            //         this.member = member;
+            //     }
+            // });
+            // this.apiService.getPermissions(this.member.id).subscribe(permissions => {
+            //     for (let group of permissions.permissions.groups) {
+            //         if (group.id == this.group_id) {
+            //             this.authService.setPermissions(group.permissions);
+            //         }
+            //         this.group = group;
+            // });
+            // this.authService.getPermissions()
+        });
+    }
+
+    isAllowed(permission: string): boolean {
+        return this.authService.isAllowed(this.group.id, permission);
+    }
+
+    // Handle Events
+    eventHandler(event: any): void {
+        console.log('Received event: ', event);
+        switch (event) {
+            // case 'updateGroupList':
+            //     this.updateGroupList();
+            //     break;
+            // case 'updateMemberList':
+            //     this.updateMemberList();
+            //     break;
+            default:
+                break;
+        }
+    }
+
+    // Update Policy List
+    updatePolicyList(): void {
+        this.policyList.updatePolicyList();
+    }
+
+    // Update Member List
+    onTabChange(event: any) {
+        this.location.replaceState(`/workspaces/${this.workspace_id}/groups/${this.group_id}/${event.tab.textLabel.toLowerCase()}`);
+    }
+}
