47495e40c982365add327a03ea298533e023c020
Michael Pisman <mike.pisman@gmail.com>
2023-12-12 22:56:35 -0700

    refactor: Added new components and removed exports
    Added PolicyListComponent, DialogViewPolicyComponent, DialogUpdatePolicyComponent

--- src/app/modules/poll-editor/poll-editor.module.ts
+++ src/app/modules/poll-editor/poll-editor.module.ts
@@ -10,6 +10,11 @@ import { PollEditorRoutingModule } from './poll-editor-routing.module';
 import { DialogAddQuestionComponent } from './components/dialog-add-question/dialog-add-question.component';
 import { DialogExitComponent } from './components/dialog-exit/dialog-exit.component';
 import { DialogComponent } from 'src/app/shared/components/dialog/dialog.component';
+import { DialogUpdatePolicyComponent } from './components/dialog-update-policy/dialog-update-policy.component';
+import { DialogViewPolicyComponent } from './components/dialog-view-policy/dialog-view-policy.component';
+import { PolicyListComponent } from './components/policy-list/policy-list.component';
+import { GridOrTableViewComponent } from 'src/app/shared/components/grid-or-table-view/grid-or-table-view.component';
+import { PageLoadingSpinnerComponent } from 'src/app/shared/components/page-loading-spinner/page-loading-spinner.component';
 
 
 @NgModule({
@@ -18,7 +23,10 @@ import { DialogComponent } from 'src/app/shared/components/dialog/dialog.compone
         DialogAddQuestionComponent,
         DialogExitComponent,
         SingleChoiceComponent,
-        MultipleChoiceComponent
+        MultipleChoiceComponent,
+        PolicyListComponent,
+        DialogViewPolicyComponent,
+        DialogUpdatePolicyComponent,
     ],
     imports: [
         CommonModule,
@@ -26,10 +34,11 @@ import { DialogComponent } from 'src/app/shared/components/dialog/dialog.compone
         ReactiveFormsModule,
         PollEditorRoutingModule,
         MarkdownModule.forChild(),
-        DialogComponent
+        DialogComponent,
+        GridOrTableViewComponent,
+        PageLoadingSpinnerComponent
     ],
     exports: [
-        PollEditorComponent
     ]
 })
 export class PollEditorModule { }
