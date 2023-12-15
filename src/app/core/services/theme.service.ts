95ba948abe2b6e6500f85642f7b5c0387e9266c9
Michael Pisman <mike.pisman@gmail.com>
2023-12-15 03:39:21 -0700

    feat: Added a service to switch themes

---
+++ src/app/core/services/theme.service.ts
@@ -0,0 +1,45 @@
+import { Injectable, OnInit, Renderer2, RendererFactory2 } from '@angular/core';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class ThemeService implements OnInit {
+
+    private _darkTheme = false;
+    private renderer: Renderer2;
+
+    constructor(rendererFactory: RendererFactory2) {
+        this.renderer = rendererFactory.createRenderer(null, null);
+    }
+
+    get activeTheme(): string {
+        return this._darkTheme ? 'dark' : 'light';
+    }
+
+    ngOnInit() {
+        this.loadTheme();
+        this.setTheme(this._darkTheme);
+    }
+
+    loadTheme() {
+        if (localStorage.getItem('activeTheme') == 'dark' || localStorage.getItem('activeTheme') == 'light') {
+            this._darkTheme = localStorage.getItem('activeTheme') == 'dark';
+        }
+    }
+
+    toggleTheme() {
+        this._darkTheme = !this._darkTheme;
+        localStorage.setItem('activeTheme', this._darkTheme ? 'dark' : 'light');
+        this.setTheme(this._darkTheme);
+      }
+
+    setTheme(darkTheme: boolean) {
+        if (darkTheme) {
+            this.renderer.removeClass(document.body, 'light-theme');
+            this.renderer.addClass(document.body, 'dark-theme');
+        } else {
+            this.renderer.removeClass(document.body, 'dark-theme');
+            this.renderer.addClass(document.body, 'light-theme');
+        }
+    }
+}
