84ea6e38ca425b0cdc22cf0369dd3c3ca401929b
Michael Pisman <mike.pisman@gmail.com>
2023-12-15 04:02:59 -0700

    refactor: Moved theme initialization to constructor

--- src/app/core/services/theme.service.ts
+++ src/app/core/services/theme.service.ts
@@ -3,34 +3,31 @@ import { Injectable, OnInit, Renderer2, RendererFactory2 } from '@angular/core';
 @Injectable({
     providedIn: 'root'
 })
-export class ThemeService implements OnInit {
+export class ThemeService {
 
     private _darkTheme = false;
     private renderer: Renderer2;
 
     constructor(rendererFactory: RendererFactory2) {
         this.renderer = rendererFactory.createRenderer(null, null);
+        this.loadTheme();
+        this.setTheme(this._darkTheme);
     }
 
     get activeTheme(): string {
         return this._darkTheme ? 'dark' : 'light';
     }
 
-    ngOnInit() {
-        this.loadTheme();
-        this.setTheme(this._darkTheme);
-    }
-
     loadTheme() {
-        if (localStorage.getItem('activeTheme') == 'dark' || localStorage.getItem('activeTheme') == 'light') {
-            this._darkTheme = localStorage.getItem('activeTheme') == 'dark';
+                if (localStorage.getItem('activeTheme') == 'dark' || localStorage.getItem('activeTheme') == 'light') {
+                        this._darkTheme = localStorage.getItem('activeTheme') == 'dark';
         }
     }
 
     toggleTheme() {
         this._darkTheme = !this._darkTheme;
         localStorage.setItem('activeTheme', this._darkTheme ? 'dark' : 'light');
-        this.setTheme(this._darkTheme);
+                this.setTheme(this._darkTheme);
       }
 
     setTheme(darkTheme: boolean) {
