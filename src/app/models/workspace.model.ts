649f0cc2613ea1e56f9ef8f8beec9226ad7cda73
Michael Pisman <mike.pisman@gmail.com>
2023-12-16 21:47:25 -0700

    style: Changed indentation

--- src/app/models/workspace.model.ts
+++ src/app/models/workspace.model.ts
@@ -4,20 +4,20 @@ import { PolicyModel } from "./policy.model";
 import { PollModel } from "./poll.model";
 
 export interface WorkspaceModel {
-  id: string;
-  name: string;
-  description: string;
-  members: MemberModel[];
-  groups: GroupModel[];
-  policies: PolicyModel[];
-  polls: PollModel[];
+    id: string;
+    name: string;
+    description: string;
+    members: MemberModel[];
+    groups: GroupModel[];
+    policies: PolicyModel[];
+    polls: PollModel[];
 }
 
 export interface WorkspaceListModel {
-  workspaces: WorkspaceModel[];
+    workspaces: WorkspaceModel[];
 }
 
 export interface createWorkspaceModel {
-  name: string;
-  description: string;
+    name: string;
+    description: string;
 }
