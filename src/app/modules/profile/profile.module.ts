8cc8e1fa05184e0d1b0759a2d97ccc2bceabe56e
Michael Pisman <mike.pisman@gmail.com>
2023-12-17 21:49:39 -0700

    feat: Added profile and profile info components to profile module

--- src/app/modules/profile/profile.module.ts
+++ src/app/modules/profile/profile.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ProfileRoutingModule } from './profile-routing.module';
+import { ProfileComponent } from './components/profile/profile.component';
+import { ProfileInfoComponent } from './components/profile-info/profile-info.component';
+import { MaterialModule } from '../material/material.module';
 
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    ProfileRoutingModule
-  ]
+    declarations: [
+        ProfileComponent,
+        ProfileInfoComponent
+    ],
+    imports: [
+        CommonModule,
+        ProfileRoutingModule,
+        MaterialModule
+    ]
 })
 export class ProfileModule { }
